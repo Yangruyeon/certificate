bbfefce10a64c16e551b9a1b8f8b66a11ccb92df
Yangruyeon <72532234+Yangruyeon@users.noreply.github.com>
2021-11-07 19:26:15 +0900

    Update certificate.sol

--- certificate.sol
+++ certificate.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "./owned.sol";
+//import "./owned.sol";
 
 contract certificate {
 
@@ -36,57 +36,102 @@ contract certificate {
 
     cert[] public Cert; //인증서 동적 배열
 
-    mapping(address ==> userinfo) public addressinfo;
+    constructor() public { //생성자. 계약서가 배포될 때 호출됨.
+        addr = msg.sender;
+    }
+    
+    mapping(address => userinfo) public addressToInfo;
+    mapping(address => cert) public certificates;
 
-    modifier onlyOwner() {
-        
+    function hasInfo(address _addr) internal {
+        if(addressToInfo[addr].id == 0 ){
+            return false;
+        }
+        return true;
     }
 
-    function setTime(uint _notBefore, uint _notAfter) public {
-
-        newUserInfo(); //함수 호출 [함수에서 함수 호출이 가능한가..?]
+    function setTime() public{ //유효기간 설정
+        notBefore = block.timestamp;
+        notAfter = notBefore + 365 days;
     }
  
     //아래 함수는 정보 생성 함수이니 private 함수로 설정한다.
-    function newUserInfo(address _addr, string _name, string _birth, uint _notBefore, uint _notAfter, byte32 ID)) private {
-        //UserInfo 배열에 정보 저장하면서 새롭게 생성
-        UserInfo.push(userinfo(_addr, _name, _birth, _notBefore, _notAfter, byte32 _ID)); 
-
-        //UserInfo 배열에 정보 추가
-        //배열이 첫 원소의 인덱스가 0이기 때문에 array.push()-1은 막 추가된 정보의 인덱스가 될 것이다.
-        //id는 NewCert 이벤트를 위해 활용한다.
-        uint id = UserInfo.push(userinfo(_addr, _name, _birth, _notBefore, _notAfter, byte32 _ID)) - 1;
-        NewCert(id, _addr, _name, _birth, _notBefore, _notAfter, byte32 _ID);
-
-        mapping(address ==> cert) public certificates;
-
-        certificates[msg.sender].caPublicKey;
-        certificates[msg.sender].userPubkey = msg.sender;
-        certificates[msg.sender].certHash = certHash;
-
-        getCertificate(); //함수 호출
-    }
-
-    //뭔가 이 함수는 return값으로 출력되는 게 있어야 할 것 같은데...
-    function getCertificate() public {
+    //상속하는 컨트랙트에서도 접근이 가능해야 하므로 internal로 변경
+    function newUserInfo(string _name, string _birth) internal {
+        addressToInfo[addr].addr = msg.sender;
+        addressToInfo[addr].name = name;
+        addressToInfo[addr].birth = birth;
+        addressToInfo[addr].notBefore = notBefore;
+        addressToInfo[addr].notAfter = notAfter;
+        addressToInfo[addr].id = setId();
         
+        certhash = keccak256(userinfoToBytes(addressToInfo[addr]));
     }
 
-
-    function hasInfo(address _addr) private {
-        if (addresstoInfo[addr] == 0) {
-            return true;
+    function userinfoToBytes(userinfo memory u) internal returns (bytes memory data){    //userinfo 구조체 내부 값들을 바이트열로 변환하여 연접
+        uint _size = 116 + bytes(u.name).length + bytes(u.birth).length;
+        bytes memory _data = new bytes(_size);
+        
+        uint counter = 0;
+        bytes memory baddr = abi.encodePacked(u.addr);
+        bytes memory bBefore = abi.encodePacked(u.notBefore);
+        bytes memory bAfter = abi.encodePacked(u.notAfter);
+        bytes memory bId = abi.encodePacked(u.id);
+        for (uint i = 0; i < 20; i++){
+            _data[counter] = bytes(baddr)[i];
+            counter++;
         }
-
-        //아래 if문은 미정 조건문
-        if (hasinfo(addr) == 0) {
-            setTime(_notBefore, _notAfter); //함수 호출
+        for (uint i = 0; i < bytes(u.name).length; i++){
+            _data[counter] = bytes(u.name)[i];
+            counter++;
         }
-
-        return false;
+        for (uint i = 0; i < bytes(u.birth).length; i++){
+            _data[counter] = bytes(u.birth)[i];
+            counter++;
+        }
+        for (uint i = 0; i < 32; i++){
+            _data[counter] = bytes(bBefore)[i];
+            counter++;
+        }
+        for (uint i = 0; i < 32; i++){
+            _data[counter] = bytes(bAfter)[i];
+            counter++;
+        }
+        for (uint i = 0; i < 32; i++){
+            _data[counter] = bytes(bId)[i];
+            counter++;
+        }
+        
+        return _data;   //연접한 바이트열 반환
+    }
+    
+    function setId() public returns(bytes32){   //현재시각+컨트랙트 호출자 정보를 이용한 랜덤 난수 생성
+        return keccak256(abi.encodePacked(block.timestamp, msg.sender));
     }
 
-    function newUserInfo(string _name, string _birth) private {
+    function newCert() public{
+        certificates[addr].caPubkey = 0x19dec5DE28cD9433d73A5FEA9C9D99E137064B57;
+        certificates[addr].userPubkey = addr;
+        certificates[addr].certhash = certhash;
+    }
+
+    function getCertificate() public view returns(address, string memory, string memory, uint, uint){   //인증서 내부 정보 반환
+        return(addressToInfo[addr].addr, addressToInfo[addr].name, addressToInfo[addr].birth, addressToInfo[addr].notBefore, addressToInfo[addr].notAfter);
+    }
+
+    function getCertInfo() public view returns(bytes32, address, address){  //인증서 해시, 이용자 공개키, 발급기관 공개키 반환
+        return(certificates[addr].certhash, certificates[addr].caPubkey, certificates[addr].userPubkey);
+    }
+    
+    function issue(string memory name, string memory birth) public {
+        if(hasinfo(addr) == false){
+            setTime();
+            newUserInfo(name, birth);
+            newCert();
+        }
+    }
+
+    function newUserInfo(string _name, string _birth) internal {
         addresstoinfo[addr].addr = msg.sender;
         addresstoinfo[addr].name = name;
         addresstoinfo[addr].birth = birth;
